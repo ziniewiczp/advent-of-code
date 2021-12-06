0e91c7499ca00bbfc9372926c0335447e3a98e6f
ziniewiczp <ziniewicz.p@gmail.com>
2021-12-06 19:21:17 +0100

    Refactor solution for day 6.

--- 2021/day-06.ts
+++ 2021/day-06.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 
-const getData = (): Array<number> => {
+const getInitialPopulation = (): Array<number> => {
     return readFileSync('./inputs/day-06.txt', 'utf8')
         .replaceAll('\r', '')
         .replaceAll('\n', '')
@@ -8,39 +8,55 @@ const getData = (): Array<number> => {
         .map((entry: string) => Number(entry));
 }
 
-const data: Array<number> = getData();
-let fishMap: Map<number, number> = new Map();
+const initializePopulationMap = (initialPopulation: Array<number>): Map<number, number> => {
+    const populationMap: Map<number, number> = new Map();
 
-data.forEach((number: number) => {
-    let currentCount = (fishMap.has(number)) ? fishMap.get(number) : 0;
-    
-    fishMap.set(number, currentCount + 1);
-});
-
-const numberOfDays: number = 256;
-
-for(let day: number = 0; day < numberOfDays; day += 1) {
-    let newFishMap: Map<number, number> = new Map();
-
-    fishMap.forEach((occurrences: number, number: number) => {
-        newFishMap.set(number - 1, occurrences);
+    initialPopulation.forEach((number: number) => {
+        const currentCount = (populationMap.has(number)) ? populationMap.get(number) : 0;
+        
+        populationMap.set(number, currentCount + 1);
     });
 
-    if(newFishMap.has(-1)) {
-        let zerosCount = newFishMap.get(-1);
-        newFishMap.delete(-1);
-        newFishMap.set(6, (newFishMap.get(6) || 0) + zerosCount);
-        newFishMap.set(8, (newFishMap.get(8) || 0) + zerosCount);
-    }
-
-    fishMap = newFishMap;
+    return populationMap;
 }
 
-let counter: number = 0;
+const simulatePopulation = (initialPopulation: Array<number>, days: number): Map<number,number> => {
+    let populationMap: Map<number, number> = initializePopulationMap(initialPopulation);
 
-fishMap.forEach((occurrences: number) => {
-    counter += occurrences;
-});
+    for(let day: number = 0; day < days; day += 1) {
+        const newPopulationMap: Map<number, number> = new Map();
+    
+        populationMap.forEach((occurrences: number, number: number) => {
+            newPopulationMap.set(number - 1, occurrences);
+        });
+    
+        if(newPopulationMap.has(-1)) {
+            const zerosCount = newPopulationMap.get(-1);
+            newPopulationMap.delete(-1);
+            newPopulationMap.set(6, (newPopulationMap.get(6) || 0) + zerosCount);
+            newPopulationMap.set(8, (newPopulationMap.get(8) || 0) + zerosCount);
+        }
+    
+        populationMap = newPopulationMap;
+    }
 
-console.log(counter);
+    return populationMap;
+}
 
+const getPopulationCount = (populationMap: Map<number, number>): number => {
+    let counter: number = 0;
+    
+    populationMap.forEach((occurrences: number) => {
+        counter += occurrences;
+    });
+
+    return counter;
+}
+
+const initialPopulation: Array<number> = getInitialPopulation();
+
+const populationMapForPart1: Map<number, number> = simulatePopulation(initialPopulation, 80);
+console.log(`Answer for part 1: ${getPopulationCount(populationMapForPart1)}`);
+
+const populationMapForPart2: Map<number, number> = simulatePopulation(initialPopulation, 256);
+console.log(`Answer for part 2: ${getPopulationCount(populationMapForPart2)}`);
