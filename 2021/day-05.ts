130a1ffa481577ab7e7378d4744e0f2cc96a7ec8
ziniewiczp <ziniewicz.p@gmail.com>
2021-12-06 19:36:20 +0100

    Refactor solution for day 5.

--- 2021/day-05.ts
+++ 2021/day-05.ts
@@ -1,63 +1,43 @@
 import { readFileSync } from 'fs';
 
-const getData = (): Array<string> => {
+const getVentsCoordinates = (): Array<string> => {
     return readFileSync('./inputs/day-05.txt', 'utf8')
         .replaceAll('\r', '')
         .split('\n');
 }
 
-const placeVents = (data: Array<string>, skipDiagonal: boolean): Map<string, number> => {
+const placeVents = (ventsCoordinates: Array<string>, skipDiagonal: boolean): Map<string, number> => {
     const vents: Map<string, number> = new Map();
 
-    for(let i: number = 0; i < data.length; i += 1) {
-        const row: string = data[i];
+    for(let i: number = 0; i < ventsCoordinates.length; i += 1) {
+        const row: string = ventsCoordinates[i];
 
         const splitRow: Array<string> = row.split(' -> ');
-        const beginning: Array<string> = splitRow[0].split(',');
-        const end: Array<string> = splitRow[1].split(',');
+        const beginning: Array<number> = splitRow[0].split(',').map(coordinate => Number(coordinate));
+        const end: Array<number> = splitRow[1].split(',').map(coordinate => Number(coordinate));
 
         const isDiagonal = beginning[0] !== end[0] && beginning[1] !== end[1];
 
-        if(isDiagonal) {
-            if(skipDiagonal) { continue; }
+        if(isDiagonal && skipDiagonal) { continue; }
 
-            let xDirection = (Number(beginning[0]) < Number(end[0])) ? 1 : -1;
-            let yDirection = (Number(beginning[1]) < Number(end[1])) ? 1 : -1;
-            let currentPoint = `${beginning[0]},${beginning[1]}`;
-            let finalPoint = `${end[0]},${end[1]}`;
+        const xDirection = (beginning[0] === end[0])
+            ? 0
+            : (beginning[0] < end[0]) ? 1 : -1;
 
-            while(currentPoint !== finalPoint) {
-                currentPoint = `${beginning[0]},${beginning[1]}`;
-                let currentPointOccurrences = vents.get(currentPoint) || 0;
-                vents.set(currentPoint, currentPointOccurrences + 1);
+        const yDirection = (beginning[1] === end[1])
+            ? 0
+            : (beginning[1] < end[1]) ? 1 : -1;
+            
+        let currentPoint = `${beginning[0]},${beginning[1]}`;
+        const finalPoint = `${end[0]},${end[1]}`;
 
-                beginning[0] = (Number(beginning[0]) + xDirection).toString();
-                beginning[1] = (Number(beginning[1]) + yDirection).toString();
-            }
-
-            // currentPoint = `${beginning[0]},${beginning[1]}`;
-            // let currentPointOccurrences = vents.get(currentPoint) || 0;
-            // vents.set(currentPoint, currentPointOccurrences + 1);
-
-        } else {
-            const isHorizontal: boolean = beginning[1] === end[1];
-            const changingCoordinate: number = (isHorizontal) ? 0 : 1;
-
-            const current = (Number(beginning[changingCoordinate]) < Number(end[changingCoordinate]))
-                ? beginning
-                : end;
-
-            while(beginning[changingCoordinate] !== end[changingCoordinate]) {
-                let currentPoint = `${current[0]},${current[1]}`;
-                let currentPointOccurrences = vents.get(currentPoint) || 0;
-                vents.set(currentPoint, currentPointOccurrences + 1);
-
-                current[changingCoordinate] = (Number(current[changingCoordinate]) + 1).toString();
-            }
-
-            let currentPoint = `${current[0]},${current[1]}`;
+        while(currentPoint !== finalPoint) {
+            currentPoint = `${beginning[0]},${beginning[1]}`;
             let currentPointOccurrences = vents.get(currentPoint) || 0;
             vents.set(currentPoint, currentPointOccurrences + 1);
+
+            beginning[0] = beginning[0] + xDirection;
+            beginning[1] = beginning[1] + yDirection;
         }
     }
 
@@ -76,11 +56,10 @@ const getOverlappingPoints = (vents: Map<string, number>): number => {
     return overlappingPoints;
 }
 
-const data: Array<string> = getData();
-const ventsWithoutDiagonals: Map<string, number> = placeVents(data, true);
-const overlappingPointsWithoutDiagonals: number = getOverlappingPoints(ventsWithoutDiagonals);
-console.log(`Answer for part 1: ${overlappingPointsWithoutDiagonals}`);
+const ventsCoordinates: Array<string> = getVentsCoordinates();
 
-const ventsWithDiagonals: Map<string, number> = placeVents(data, false);
-const overlappingPointsWithDiagonals: number = getOverlappingPoints(ventsWithDiagonals);
-console.log(`Answer for part 2: ${overlappingPointsWithDiagonals}`);
+const ventsWithoutDiagonals: Map<string, number> = placeVents(ventsCoordinates, true);
+console.log(`Answer for part 1: ${getOverlappingPoints(ventsWithoutDiagonals)}`);
+
+const ventsWithDiagonals: Map<string, number> = placeVents(ventsCoordinates, false);
+console.log(`Answer for part 2: ${getOverlappingPoints(ventsWithDiagonals)}`);
