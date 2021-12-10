1b0480739662096a96780fb996b8f7a32250896e
Piotr Ziniewicz <piotr.ziniewicz@contracted.pmi.com>
2021-12-10 17:37:19 +0100

    Refactor solution for the day 10.

--- 2021/day-10.ts
+++ 2021/day-10.ts
@@ -1,97 +1,77 @@
 import { readFileSync } from 'fs';
 
-const openingBrackets: Map<string, string> = new Map([
-    ['(', ')'],
-    ['[', ']'],
-    ['{', '}'],
-    ['<', '>']]);
-
 const getSubsystem = (): Array<string> => {
     return readFileSync('./inputs/day-10.txt', 'utf8')
         .replaceAll('\r', '')
         .split('\n');
 }
 
-const removeCorruptedLines = ((corruptedSubsystem: Array<string>): Array<string> => {
-    let syntaxErrorScore = 0;
-    const subsystem: Array<string> = new Array();
-
-    const closingBrackets: Map<string, number> = new Map([
+const calculateScores = ((subsystem: Array<string>): void => {
+    const bracketsMapping: Map<string, string> = new Map([
+        ['(', ')'],
+        ['[', ']'],
+        ['{', '}'],
+        ['<', '>']]);
+    
+    const corruptionScoreMapping: Map<string, number> = new Map([
         [')', 3],
         [']', 57],
         ['}', 1197],
         ['>', 25137,]]);
-
-    const openedBrackets: Array<string> = new Array();
-
-    for(let i: number = 0; i < corruptedSubsystem.length; i += 1) {
-        const line = corruptedSubsystem[i].split('');
-        let isLineCorrupted = false;
+    
+    const completionScoreMapping: Map<string, number> = new Map([
+        [')', 1],
+        [']', 2],
+        ['}', 3],
+        ['>', 4,]]);
+    
+    let syntaxErrorScore = 0;
+    const lineCompletionScores: Array<number> = new Array();
+    
+    for(let i: number = 0; i < subsystem.length; i += 1) {
+        const line = subsystem[i].split('');
+        let isLineCorrupted: boolean = false;
+    
+        let openedBrackets: Array<string> = new Array();
     
         for(let j: number = 0; j < line.length; j += 1) {
-            if(openingBrackets.has(line[j])) {
+            if(bracketsMapping.has(line[j])) {
                 openedBrackets.push(line[j]);
             
             } else {
                 const currentOpenBracket = openedBrackets.pop();
-                if(openingBrackets.get(currentOpenBracket) !== line[j]) {
-                    syntaxErrorScore += closingBrackets.get(line[j]);
+                if(bracketsMapping.get(currentOpenBracket) !== line[j]) {
+                    syntaxErrorScore += corruptionScoreMapping.get(line[j]);
                     isLineCorrupted = true;
                     break;
                 }
             }
         }
     
-        if(!isLineCorrupted) { subsystem.push(line.join('')); }
+        if(!isLineCorrupted) {
+            let totalScore = 0;
+    
+            while(openedBrackets.length > 0) {
+                const currentOpenBracket = openedBrackets.pop();
+                const matchingClosingBracket = bracketsMapping.get(currentOpenBracket);
+                line.push(matchingClosingBracket);
+    
+                totalScore *= 5;
+                totalScore += completionScoreMapping.get(matchingClosingBracket);
+            }
+    
+            lineCompletionScores.push(totalScore);
+        }
     }
     
     console.log(`Answer for part 1: ${syntaxErrorScore}`);
-
-    return subsystem;
+    
+    lineCompletionScores.sort((a,b) => a-b);
+    const n: number = lineCompletionScores.length;
+    const middleScore = lineCompletionScores[Math.floor(n / 2)];
+    
+    console.log(`Answer for part 2: ${middleScore}`);
 });
 
-const corruptedSubsystem: Array<string> = getSubsystem();
-const subsystem: Array<string> = removeCorruptedLines(corruptedSubsystem);
-const lineScores: Array<number> = new Array();
-
-for(let i: number = 0; i < subsystem.length; i += 1) {
-    const line = subsystem[i].split('');
-
-    const closingBrackets: Map<string, number> = new Map([
-        [')', 1],
-        [']', 2],
-        ['}', 3],
-        ['>', 4,]]);
-
-    const openedBrackets: Array<string> = new Array();
-
-    for(let j: number = 0; j < line.length; j += 1) {
-        if(openingBrackets.has(line[j])) {
-            openedBrackets.push(line[j]);
-        
-        } else {
-            openedBrackets.pop();
-        }
-    }
-
-    let totalScore = 0;
-
-    while(openedBrackets.length > 0) {
-        const currentOpenBracket = openedBrackets.pop();
-        const matchingClosingBracket = openingBrackets.get(currentOpenBracket);
-        line.push(matchingClosingBracket);
-
-        totalScore *= 5;
-        totalScore += closingBrackets.get(matchingClosingBracket);
-    }
-
-    lineScores.push(totalScore);
-}
-
-lineScores.sort((a,b) => a-b);
-
-console.log(lineScores);
-const n: number = lineScores.length;
-const middleScore = lineScores[Math.floor(n / 2)];
-
-console.log(`Answer for part 2: ${middleScore}`);
+const subsystem: Array<string> = getSubsystem();
+calculateScores(subsystem);
